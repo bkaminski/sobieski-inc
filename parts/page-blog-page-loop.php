95eaa93bb4d9a1bb7b4ea547c3041f8b25e30751
Ben Kaminski <ben@benkaminski.com>
2020-07-20 16:36:30 -0400

    added breakpoints on blog flexbox

--- parts/page-blog-page-loop.php
+++ parts/page-blog-page-loop.php
@@ -15,24 +15,26 @@
 				$i = 0;
 				while ($my_query->have_posts()) : $my_query->the_post();
 					if($i % 3 == 0) { ?>
+
+						<div class="row">
 						
-						<div class="card-deck">
 						<?php } ?>
-
-						<div class="card mb-4 shadow">
-							<?php  $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
-
-							<img src="<?php echo $featImg['0'];?>" class="card-img-top" alt="">
-							<div class="card-body">
-								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-									<h1 class="card-title"><?php the_title(); ?></h1>
-								</a>
-								<div class="card-text pb-5">
-									<?php the_excerpt(); ?>
-										
+						
+							<div class="col-lg-4 mb-4">
+								<div class="card shadow h-100">
+									<?php  $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+									<img src="<?php echo $featImg['0'];?>" class="card-img-top" alt="">
+									<div class="card-body">
+										<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+											<h1 class="card-title"><?php the_title(); ?></h1>
+										</a>
+										<div class="card-text pb-5">
+											<?php the_excerpt(); ?>
+												
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
 							<?php $i++; if($i != 0 && $i % 3 == 0) { ?>
 
 						</div>
