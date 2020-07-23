589b928774fb96a1b747f4fd5144f89e210beb32
Ben Kaminski <ben@benkaminski.com>
2020-07-22 21:25:45 -0400

    adjusted affiliate logos for all devices

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,3 +28,28 @@ if ($('.blog')[0]){
 }
 
 })( jQuery );
+
+(function( $ ) {
+	$( document ).ready(function() {
+
+	// RESPONSIVE CONTROLS
+	var $window = $(window);
+	function checkWidth() {
+
+		if ($window.width() < 768) {
+			$('.center-on-mobile1').addClass('text-center');
+			$('.center-on-mobile2').addClass('text-center');
+			$('.affiliate-row').find('img').addClass('mt-2');
+		};
+		if ($window.width() >= 769) {
+			$('.center-on-mobile1').removeClass('text-center').addClass('text-right');
+			$('.center-on-mobile2').removeClass('text-center').addClass('text-left');
+		}
+	}
+	
+	checkWidth();
+	$(window).resize(checkWidth);
+	
+	// END RESPONSIVE CONTROL
+	});	
+})( jQuery );
--- parts/page-affiliates.php
+++ parts/page-affiliates.php
@@ -1,76 +1,76 @@
 <div class="affiliates">
-	<div class="container pt-5 pb-4">
-		<div class="row pb-2 text-center">
-			<div class="col-sm-2">
+	<div class="container pt-5">
+		<div class="row pb-2 affiliate-row text-center">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid pt-4" src="' . esc_url( $uploads['baseurl'] . '/2020/07/rses.gif' ) . '" alt="" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/phcc_logo.gif' ) . '" alt="" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/10year-solid-border_0.png' ) . '" alt="" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/ncilogo.jpg' ) . '" alt="" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/logo-nate.gif' ) . '" alt="NATE Certified logo" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/bbblogo2.gif' ) . '" alt="" />' 
 				;?>
 
 			</div>
 		</div>
-		<div class="row pb-2 text-center">
-			<div class="col-sm-2">
+		<div class="row pb-2 text-center affiliate-row">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/home_advisor_logo.jpg' ) . '" alt="HomeAdvisor Screened & Approved Contractor Logo" />' 
 				;?>
 				
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/step-logo_1_orig.png' ) . '" alt="" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/angies-list-award-solar-san-diego.jpg' ) . '" alt="Angie’s List 2013 Super Service Award logo" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/hpac_da-award_2013_logo.jpg' ) . '" alt="HPAC Engineering Design Awards 2013 logo" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/new-linc-logo.png' ) . '" alt="" />' 
 				;?>
 
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/26856ac9_picture1.jpg' ) . '" alt="Frost & Sullivan Best Practices Award logo" />' 
 				;?>
 
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-sm-6 text-right">
+		<div class="row affiliate-row">
+			<div class="center-on-mobile1 col-xs-6 col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/dcalogo_side_full_300rgb_trans.png' ) . '" alt="" />' 
 				;?>
 				
 			</div>
-			<div class="col-sm-6">
+			<div class="center-on-mobile2 col-xs-6 col-sm">
 				<?php $uploads = wp_upload_dir(); echo '<img class="img-fluid" src="' . esc_url( $uploads['baseurl'] . '/2020/07/abcga-logo-editible-file-20101.jpg' ) . '" alt="" />' 
 				;?>
 
