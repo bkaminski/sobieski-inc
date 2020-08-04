b5de6c11a85c1b4f2c6fdf5d799b00886cd018b3
Ben Kaminski <ben@benkaminski.com>
2020-08-04 13:32:48 -0400

    working on career page

--- page-careers.php
+++ page-careers.php
@@ -9,9 +9,18 @@
 get_header(); ?>
 
 <article>
-	<?php get_template_part( 'parts/page', 'blog-loop' ); ?>
+	<?php get_template_part( 'parts/page', 'cpt-blog-loop' ); ?>
 
 </article>
+<main class="career-hero">
+	<?php get_template_part( 'parts/page', 'career-hero' ); ?>
 
+</main>
+
+<?php get_template_part( 'parts/page', 'contact-us' ); ?>
+
+<?php get_template_part( 'parts/page', 'home-header' ); ?>
+
+<?php get_template_part( 'parts/page', 'affiliates' ); ?>
 
 <?php get_footer(); ?>
---
+++ parts/page-career-hero.php
@@ -0,0 +1,24 @@
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6">
+				<h2 class="text-white text-uppercase font-weight-bold" style="font-size:40px;">Work with a company that cares about you and your success</h2>
+				<p>Large company stability with a small family business feel</p>
+				<a class="btn btn-outline-sobieski-white btn-lg rounded-0 pt-3 pb-3" href="#">View Jobs <i class="fas fa-arrow-right ml-3"></i></a>
+			</div>
+			<div class="col-lg-6 pr-0">
+				<div id="careerCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="<?php the_field('image_one'); ?>" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="<?php the_field('image_two'); ?>" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="<?php the_field('image_three'); ?>" class="d-block w-100" alt="...">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
