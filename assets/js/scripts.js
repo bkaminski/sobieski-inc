e2ebd58c4518a0bac454248bdb4707f8b9a88d9b
Ben Kaminski <ben@benkaminski.com>
2020-08-06 10:38:28 -0400

    finished logic for career page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,38 @@
 (function( $ ) {
 
 $(document).ready(function(){
-    $(".career-baltimore, .career-delaware, .career-executive").hide();
+    $(".career-baltimore, .career-delaware, .career-executive, .career-kop, .career-nj, .career-philadelphia, .career-south-delaware, .career-york").hide();
     $("#baltimoreCareer").on("click",function(){
         $(".career-baltimore").show();
-        $(".career-delaware, .career-executive").hide();
+        $(".career-delaware, .career-executive, .career-kop, .career-nj, .career-philadelphia, .career-south-delaware, .career-york").hide();
     });
     $("#delawareCareer").on("click",function(){
         $(".career-delaware").show();
-        $(".career-baltimore, .career-executive").hide();
+        $(".career-baltimore, .career-executive, .career-kop, .career-nj, .career-philadelphia, .career-south-delaware, .career-york").hide();
     });
     $("#executiveCareer").on("click",function(){
         $(".career-executive").show();
-        $(".career-baltimore, .career-delaware").hide();
+        $(".career-baltimore, .career-delaware, .career-kop, .career-nj, .career-philadelphia, .career-south-delaware, .career-york").hide();
+    });
+    $("#kopCareer").on("click",function(){
+        $(".career-kop").show();
+        $(".career-baltimore, .career-delaware, .career-executive, .career-nj, .career-philadelphia, .career-south-delaware, .career-york").hide();
+    });
+    $("#njCareer").on("click",function(){
+        $(".career-nj").show();
+        $(".career-baltimore, .career-delaware, .career-executive, .career-kop, .career-philadelphia, .career-south-delaware, .career-york").hide();
+    });
+    $("#phillyCareer").on("click",function(){
+        $(".career-philadelphia").show();
+        $(".career-baltimore, .career-delaware, .career-executive, .career-kop, .career-nj, .career-south-delaware, .career-york").hide();
+    });
+     $("#southDelawareCareer").on("click",function(){
+        $(".career-south-delaware").show();
+        $(".career-baltimore, .career-delaware, .career-executive, .career-kop, .career-nj, .career-philadelphia, .career-york").hide();
+    });
+      $("#yorkCareer").on("click",function(){
+        $(".career-york").show();
+        $(".career-baltimore, .career-delaware, .career-executive, .career-kop, .career-nj, .career-philadelphia, .career-south-delaware").hide();
     });
 });
 
--- parts/page-current-openings.php
+++ parts/page-current-openings.php
@@ -5,11 +5,11 @@
 			<div class="col"><a class="career-link" id="baltimoreCareer">Baltimore</a></div>
 			<div class="col"><a class="career-link" id="delawareCareer">Delaware</a></div>
 			<div class="col"><a class="career-link" id="executiveCareer">Executive</a></div>
-			<div class="col">King of Prussia</div>
-			<div class="col">New Jersey</div>
-			<div class="col">Philadelphia</div>
-			<div class="col">South Delaware</div>
-			<div class="col">York, PA</div>
+			<div class="col"><a class="career-link" id="kopCareer">King of Prussia</a></div>
+			<div class="col"><a class="career-link" id="njCareer">New Jersey</a></div>
+			<div class="col"><a class="career-link" id="phillyCareer">Philadelphia</a></div>
+			<div class="col"><a class="career-link" id="southDelawareCareer">South Delaware</a></div>
+			<div class="col"><a class="career-link" id="yorkCareer">York, PA</div>
 		</div>
 	</div>
 </div>
@@ -47,13 +47,13 @@
 				<?php } ?>
 			<?php endwhile; } wp_reset_query(); ?>
 	</div>
-</div>
+	</div>
 	<!-- Delaware -->
 	<div class="career-delaware">
 		<h4 class="text-center text-uppercase">Delaware</h4>
 		<?php $args=array(
 			'post_type' => 'careers',
-			'posts_per_page' => 20,
+			'posts_per_page' => 27,
 			'category_name' => 'delaware',
 		);
 		$my_query = null;
@@ -70,7 +70,7 @@
 								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
 								<div class="card-text text-center">
 									<?php the_content(); ?>
-										
+											
 								</div>
 							</div>
 						</div>
@@ -80,8 +80,8 @@
 					<div class="clearfix"></div>
 				<?php } ?>
 			<?php endwhile; } wp_reset_query(); ?>
+		</div>
 	</div>
-</div>
 	<!-- Executive -->
 	<div class="career-executive">
 		<h4 class="text-center text-uppercase">Executive</h4>
@@ -104,6 +104,74 @@
 								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
 								<div class="card-text text-center">
 									<?php the_content(); ?>
+											
+								</div>
+								</div>
+							</div>
+						</div>
+						<?php $i++; if($i != 0 && $i % 3 == 0) { ?>
+						</div>
+						<div class="clearfix"></div>
+					<?php } ?>
+				<?php endwhile; } wp_reset_query(); ?>
+		</div>
+	</div>
+	<!-- King of Prussia -->
+	<div class="career-kop">
+		<h4 class="text-center text-uppercase">King of Prussia</h4>
+		<?php $args=array(
+			'post_type' => 'careers',
+			'posts_per_page' => 20,
+			'category_name' => 'king-of-prussia',
+		);
+		$my_query = null;
+		$my_query = new WP_Query($args);
+		if( $my_query->have_posts() ) {
+			$i = 0;
+			while ($my_query->have_posts()) : $my_query->the_post();
+				if($i % 3 == 0) { ?>
+					<div class="row">
+					<?php } ?>
+					<div class="col-lg-4 mb-4">
+						<div class="card shadow h-100">
+							<div class="card-body p-0">
+								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
+								<div class="card-text text-center">
+									<?php the_content(); ?>
+										
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php $i++; if($i != 0 && $i % 3 == 0) { ?>
+					</div>
+					<div class="clearfix"></div>
+				<?php } ?>
+			<?php endwhile; } wp_reset_query(); ?>
+		</div>
+	</div>
+	<!-- New Jersey -->
+	<div class="career-nj">
+		<h4 class="text-center text-uppercase">New Jersey</h4>
+		<?php $args=array(
+			'post_type' => 'careers',
+			'posts_per_page' => 20,
+			'category_name' => 'new-jersey',
+		);
+		$my_query = null;
+		$my_query = new WP_Query($args);
+		if( $my_query->have_posts() ) {
+			$i = 0;
+			while ($my_query->have_posts()) : $my_query->the_post();
+				if($i % 3 == 0) { ?>
+					<div class="row">
+					<?php } ?>
+					<div class="col-lg-4 mb-4">
+						<div class="card shadow h-100">
+							<div class="card-body p-0">
+								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
+								<div class="card-text text-center">
+									<?php the_content(); ?>
 										
 								</div>
 							</div>
@@ -116,7 +184,108 @@
 			<?php endwhile; } wp_reset_query(); ?>
 	</div>
 </div>
-			
+	<!-- Philadelphia -->
+	<div class="career-philadelphia">
+		<h4 class="text-center text-uppercase">Philadelphia</h4>
+		<?php $args=array(
+			'post_type' => 'careers',
+			'posts_per_page' => 20,
+			'category_name' => 'philadelphia',
+		);
+		$my_query = null;
+		$my_query = new WP_Query($args);
+		if( $my_query->have_posts() ) {
+			$i = 0;
+			while ($my_query->have_posts()) : $my_query->the_post();
+				if($i % 3 == 0) { ?>
+					<div class="row">
+					<?php } ?>
+					<div class="col-lg-4 mb-4">
+						<div class="card shadow h-100">
+							<div class="card-body p-0">
+								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
+								<div class="card-text text-center">
+									<?php the_content(); ?>
+										
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php $i++; if($i != 0 && $i % 3 == 0) { ?>
+					</div>
+					<div class="clearfix"></div>
+				<?php } ?>
+			<?php endwhile; } wp_reset_query(); ?>
+	</div>
+</div>	
+	<!-- South Delaware -->
+	<div class="career-south-delaware">
+		<h4 class="text-center text-uppercase">South Delaware</h4>
+		<?php $args=array(
+			'post_type' => 'careers',
+			'posts_per_page' => 20,
+			'category_name' => 'south-delaware',
+		);
+		$my_query = null;
+		$my_query = new WP_Query($args);
+		if( $my_query->have_posts() ) {
+			$i = 0;
+			while ($my_query->have_posts()) : $my_query->the_post();
+				if($i % 3 == 0) { ?>
+					<div class="row">
+					<?php } ?>
+					<div class="col-lg-4 mb-4">
+						<div class="card shadow h-100">
+							<div class="card-body p-0">
+								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
+								<div class="card-text text-center">
+									<?php the_content(); ?>
+										
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php $i++; if($i != 0 && $i % 3 == 0) { ?>
+					</div>
+					<div class="clearfix"></div>
+				<?php } ?>
+			<?php endwhile; } wp_reset_query(); ?>
+	</div>
+</div>			
+<!-- York PA -->
+	<div class="career-york">
+		<h4 class="text-center text-uppercase">York, PA</h4>
+		<?php $args=array(
+			'post_type' => 'careers',
+			'posts_per_page' => 20,
+			'category_name' => 'york-pa',
+		);
+		$my_query = null;
+		$my_query = new WP_Query($args);
+		if( $my_query->have_posts() ) {
+			$i = 0;
+			while ($my_query->have_posts()) : $my_query->the_post();
+				if($i % 3 == 0) { ?>
+					<div class="row">
+					<?php } ?>
+					<div class="col-lg-4 mb-4">
+						<div class="card shadow h-100">
+							<div class="card-body p-0">
+								<p class="card-title text-center pt-2"><?php the_title(); ?></p>
+								<div class="card-text text-center">
+									<?php the_content(); ?>
+										
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php $i++; if($i != 0 && $i % 3 == 0) { ?>
+					</div>
+					<div class="clearfix"></div>
+				<?php } ?>
+			<?php endwhile; } wp_reset_query(); ?>
+	</div>
+</div>			
 		
 
 		
