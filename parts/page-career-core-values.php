65261cb6b05ffe9a69d29ef9635e628220fe8b7a
Ben Kaminski <ben@benkaminski.com>
2020-08-08 20:14:14 -0400

    added core values to careers

--- parts/page-career-core-values.php
+++ parts/page-career-core-values.php
@@ -1,18 +1,29 @@
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-lg-4 pl-0">
-			<div class="core-value">
+<div class="career-values">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 integrity">
+				<div class="core-value text-center pt-5">
+					<?php $uploads = wp_upload_dir(); echo '<img src="' . esc_url( $uploads['baseurl'] . '/2020/08/integrity.png' ) . '" class="pb-4" alt="Graphic representing integrity" height="120" width="100" >'; ?>
 
+					<h2 class="text-uppercase values-title">Integrity</h2>
+					<p class="text-white"><small>Making the right choice even when no one is looking.</small></p>
+				</div>
 			</div>
-		</div>
-		<div class="col-lg-4 pr-1 pl-1">
-			<div class="core-value">
+			<div class="col-lg-4 family">
+				<div class="core-value text-center pt-5">
+					<?php $uploads = wp_upload_dir(); echo '<img src="' . esc_url( $uploads['baseurl'] . '/2020/08/family.png' ) . '" class="pb-4" alt="Graphic representing integrity" height="120" width="120" >'; ?>
 
+					<h2 class="text-uppercase values-title">Family</h2>
+					<p class="text-white"><small>Leaders who understand how their employees are truly motivated, both personally and professionally.</small></p>
+				</div>
 			</div>
-		</div>
-		<div class="col-lg-4 pr-0">
-			<div class="core-value">
+			<div class="col-lg-4 courage">
+				<div class="core-value text-center pt-5">
+					<?php $uploads = wp_upload_dir(); echo '<img src="' . esc_url( $uploads['baseurl'] . '/2020/08/fist.png' ) . '" class="pb-4" alt="Graphic representing integrity" height="120" width="100" >'; ?>
 
+					<h2 class="text-uppercase values-title">Courage</h2>
+					<p class="text-white"><small>To always do the right thing, whether working with a customer or fellow employee.</small></p>
+				</div>
 			</div>
 		</div>
 	</div>
