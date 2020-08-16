06dc47925980c06eb2658898f30bf0d45109b852
Ben Kaminski <ben@benkaminski.com>
2020-08-16 15:33:18 -0400

    checked for missing image on random posts

--- parts/page-rand-posts.php
+++ parts/page-rand-posts.php
@@ -2,16 +2,15 @@
 	<div class="container pt-5">
 		<div class="row">
 			<?php query_posts('orderby=rand&posts_per_page=3'); if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<?php $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 
 			<div class="col-lg card-deck mb-5">
 				<div class="card shadow">
-					<figure class="boxImg">
-						<a href="<?php the_permalink(); ?>">
-							<?php  $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
-							
-							<img src="<?php echo $featImg['0'];?>" class="card-img-top" alt="">		
-						</a>
-					</figure>
+					<a href="<?php the_permalink(); ?>">
+						<figure class="boxImg"
+							<?php if( empty( $featImg ) ) { $featImg = ''; } if( $featImg ):?> style="background: url('<?php echo $featImg['0'];?>');background-size: cover;height: 150px;"><?php endif;?>
+						</figure>
+					</a>
 					<div class="card-body">
 						<h3 class="card-title main-header-left"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 						<div class="mb-3">
